4afa2926371a5ade19d067b179d386b881da5ba0
Gabriel Costa Martins Ganassin <gabricostam@gmail.com>
2025-03-12 13:41:32 -0300

    initial setup
    - kubernetes-dashboard
    - ingress-nginx

---
+++ ingress-nginx/install.sh
@@ -0,0 +1,5 @@
+helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
+  --namespace ingress-nginx --create-namespace \
+  --set controller.service.type=NodePort \
+  --set controller.hostNetwork=true \
+  --set controller.allowSnippetAnnotations=true
---
+++ kubernetes-dashboard/install.sh
@@ -0,0 +1,5 @@
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard \
+        --create-namespace --namespace kubernetes-dashboard
+
