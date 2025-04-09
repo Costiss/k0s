6517d599d0885128debd3cd8442959fb0dfebafa
Gabriel Costa Martins Ganassin <gabricostam@gmail.com>
2025-04-08 23:48:53 -0300

    refact: migrated to CloudNativePG

--- postgres/deploy.sh
+++ postgres/deploy.sh
@@ -5,8 +5,4 @@ cd postgres
 
 export $(grep -v '^#' ../.env | xargs)
 
-POSTGRES_PASSWORD=$(echo -n $POSTGRES_PASSWORD | base64)
-
 envsubst < deployment.yaml | kubectl apply -f -
-
-#postgres.postgres.svc.cluster.local:5432
