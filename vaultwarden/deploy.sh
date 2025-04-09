c6e179a7427dfd273bcc6f7fc071e45ef934d9bc
Gabriel Costa Martins Ganassin <gabricostam@gmail.com>
2025-04-08 23:48:27 -0300

    chore: added deployment restart on end

--- vaultwarden/deploy.sh
+++ vaultwarden/deploy.sh
@@ -8,5 +8,6 @@ export $(grep -v '^#' ../.env | xargs) #load envs
 VAULTWARDEN_ADMIN_TOKEN=$(echo -n $VAULTWARDEN_ADMIN_TOKEN | base64)
 VAULTWARDEN_DATABASE_URL=$(echo -n $VAULTWARDEN_DATABASE_URL | base64 -w 0)
 
-
 envsubst < deployment.yaml | kubectl apply -f -
+
+kubectl rollout restart deployment vaultwarden -n vaultwarden
