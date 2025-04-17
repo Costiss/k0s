d2aebddd876678df38036c8a680961010c0fb927
Gabriel Costa Martins Ganassin <gabricostam@gmail.com>
2025-04-17 13:41:31 -0300

    feat: added superuser secret

--- postgres/deploy.sh
+++ postgres/deploy.sh
@@ -5,4 +5,8 @@ cd postgres
 
 export $(grep -v '^#' ../.env | xargs)
 
+export POSTGRES_PASSWORD=$(echo -n $POSTGRES_PASSWORD | base64 -w 0)
+export POSTGRES_USER=$(echo -n postgres | base64 -w 0)
+
 envsubst < deployment.yaml | kubectl apply -f -
+
